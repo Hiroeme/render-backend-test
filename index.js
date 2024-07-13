280e182f754bdc813089301a6aae7a5aacd4e08e
Hiroeme <83380500+Hiroeme@users.noreply.github.com>
2024-07-12 20:44:30 -0700

    updated

--- index.js
+++ index.js
@@ -21,13 +21,9 @@ let notes = [
 ]
 
 app.use(cors())
-
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
 
 app.get('/api/notes', (request, response) => {
   response.json(notes)
